0112063ac433060a17741786b5e60d32f25a2880
Yuanzhe LI <59906908+PatrickYLi@users.noreply.github.com>
2023-07-07 17:54:56 +0900

    Update LAD regression.py

--- LAD regression.py
+++ LAD regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 from skbonus.linear_model import LADRegression
+from joblib import Parallel, delayed
 
 # Read csv file
 conc = np.genfromtxt("Conc.txt")
@@ -13,31 +14,38 @@ r2_values = np.zeros((abso.shape[0],1))
 # Intializa slope array
 slopes = np.zeros((abso.shape[0], 1))
 
-# For each line of abso
-for i in range(abso.shape[0]):
-    
+# Define a function for the computation that needs to be parallelized
+def compute(i):
     df = pd.DataFrame({'Concentration': conc, 'Absorbance': abso[i,:]})
     df = df.dropna()
     
     if df.empty or len(df) < 2:
-        r2_values[i] = 0
+        return 0, 0
     else:
         # from df to np
         x = df['Concentration'].values.reshape(-1,1)
         y = df['Absorbance'].values
         
-        # LAD linear regressuib, pass origin point
+        # LAD linear regression, pass origin point
         model = LADRegression(fit_intercept=False)
         model.fit(x, y)
     
         # R2
-        r2_values[i] = r2_score(y, model.predict(x))
+        r2 = r2_score(y, model.predict(x))
         
-        # 保存斜率
-        slopes[i] = model.coef_
-    
-# save result
-np.savetxt("r2_values.txt", r2_values, delimiter=",")
-np.savetxt('Slope.txt', slopes)
+        # Save slope
+        slope = model.coef_
+        
+        return r2, slope
+
+# Use joblib to parallelize the loop
+results = Parallel(n_jobs=-1)(delayed(compute)(i) for i in range(abso.shape[0])) # n_jobs=-1， use all threads
+
+# Unpack results
+r2_values, slopes = zip(*results)
+
+# Save result
+np.savetxt("r2_values_multi.txt", r2_values, delimiter=",")
+np.savetxt('Slope_multi.txt', slopes)
 
 print("done!")
